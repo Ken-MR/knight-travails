9d820fccd3c1f4b1fda7441dfa74473a46a3ad03
KenMR <kmr5591@gmail.com>
2023-03-08 20:33:32 -0500

    Create a function to track how many moves it takes for the knight to get from start to end

--- knight.js
+++ knight.js
@@ -2,7 +2,8 @@ class Space {
   // create a new space and parameters to track whether it was visited, its coordinates,
   // its distance from a starting point, and what valid destinations are possible from it
   constructor(coordinates) {
-    this.visited = null; this.distance = null;
+    this.visited = null;
+    this.distance = null;
     this.space = coordinates;
     this.destinations = this.calculateDestinations(coordinates);
   }
@@ -47,6 +48,45 @@ class ChessBoard {
     }
     return board;
   }
+
+  knightMoves(start, end) {
+    // create queue that will check spaces that will be moved to next
+    let queue = [];
+    queue.push(start);
+
+    // set start position of board to visited so it isn't checked for movement later
+    this.board[start[0]][start[1]].visited = true;
+
+    // log start and end position of knight
+    this.start = start;
+    this.end = end;
+
+    // set initial position of knight to zero distance
+    this.board[start[0]][start[1]].distance = 0;
+
+    // run through all possible moves until the end point is found
+    do {
+      // retrieve values of the item currently queued for checking
+      let currentLocation = queue[0];
+      let Futuredestinations = this.board[currentLocation[0]][currentLocation[1]].destinations;
+      let currentDistance = this.board[currentLocation[0]][currentLocation[1]].distance;
+      if ((currentLocation[0] === end[0]) && (currentLocation[1] === end[1])) {
+        return console.log(`Found in ${currentDistance} moves!`);
+      }
+      for (let i = 0; i < Futuredestinations.length; i++) {
+        // if space hasn't been visited yet perform necessary actions
+        if (!this.board[Futuredestinations[i][0]][Futuredestinations[i][1]].visited)  {
+          // add destinations of current positions to the queue
+          queue.push(this.board[Futuredestinations[i][0]][Futuredestinations[i][1]].space);
+          // make sure this location will be one greater than current distance
+          this.board[Futuredestinations[i][0]][Futuredestinations[i][1]].distance = currentDistance + 1;
+          // mark as visited to show it is being queued and won't be queued again
+          this.board[Futuredestinations[i][0]][Futuredestinations[i][1]].visited = true;
+        }
+      }
+      queue.shift();
+    } while (queue.length > 0);
+  }
 }
   
 let board = new ChessBoard();
@@ -55,5 +95,6 @@ console.log(board.board[0]);
 console.log(board.board[0][0]);
 console.log(board.board[0][5]);
 console.log(board.board[0][7]);
-
 console.log(board.board[4][4]);
+
+board.knightMoves([0,0],[6,7]);
